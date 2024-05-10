fee3d4a5db76acbb1c007dcce8531c42eb194b84
Nachi-on-git <nachiket.labade@apptware.com>
2024-05-10 23:22:48 +0530

    design system added

---
+++ flutter_mavericks/lib/design_system/scalesystem.dart
@@ -0,0 +1,6 @@
+abstract class ScaleSystem {
+  static const double scale1 = 1.0;
+  static const double scale2 = 2.0;
+  static const double scale3 = 3.0;
+  static const double scale4 = 4.0;
+}
---
+++ flutter_mavericks/lib/design_system/sizesystem.dart
@@ -0,0 +1,67 @@
+//This file will be used to maintain the sizes throughout the project.
+
+abstract class SizeSystem {
+  static const double size306 = 3.9;
+  static const double size2 = 2.0;
+  static const double size3 =3.0;
+  static const double size4 = 4.0;
+  static const double size5 = 5.0;
+  static const double size6 = 6.0;
+  static const double size8 = 8.0;
+  static const double size806 = 8.6;
+  static const double size9 = 9.0;
+  static const double size10 = 10.0;
+  static const double size12 = 12.0;
+  static const double size13 =13.0;
+  static const double size14 = 14.0;
+  static const double size15 = 15.0;
+  static const double size16 = 16.0;
+  static const double size17 =17.0;
+  static const double size18 = 18.0;
+  static const double size19 = 19.0;
+  static const double size1806 = 18.6;
+  static const double size20 = 20.0;
+  static const double size21 = 21.0;
+  static const double size22 = 22.0;
+  static const double size23 =23.0;
+  static const double size24 = 24.0;
+  static const double size25 =25.0;
+  static const double size2508 = 25.8;
+  static const double size26 = 26.0;
+  static const double size28 = 28.0;
+  static const double size30 = 30.0;
+  static const double size32 = 32.0;
+  static const double size34 = 34.0;
+  static const double size36 = 36.0;
+  static const double size38 = 38.0;
+  static const double size40 = 40.0;
+  static const double size44 = 44.0;
+  static const double size45 = 45.0;
+
+  static const double size48 = 48.0;
+  static const double size52 = 52.0;
+  static const double size50 = 50.0;
+  static const double size55 = 55.0;
+  static const double size60 = 60.0;
+  static const double size66 = 66.0;
+  static const double size78 = 78.0;
+  static const double size80 = 80.0;
+  static const double size85 = 85.0;
+  static const double size87 = 87.0;
+  static const double size90 = 90.0;
+  static const double size96 = 96.0;
+  static const double size100 = 100.0;
+  static const double size104 = 104.0;
+  static const double size123 = 123.0;
+  static const double size126 = 126.0;
+
+  static const double size150 = 150.0;
+  static const double size200 = 200.0;
+  static const double size300 = 300.0;
+  static const int duration200 = 200;
+  static const int duration300 = 300;
+  static const int duration400 = 400;
+  static const int duration500 = 500;
+  static const int duration700 = 700;
+  static const int duration800 = 800;
+}
