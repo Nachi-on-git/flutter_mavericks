d3f2a322e58e6c7ddf1b266dc77a442c22e9f27c
Nachi-on-git <nachiket.labade@apptware.com>
2024-05-12 02:02:10 +0530

    worked on excel sheet

---
+++ flutter_mavericks/lib/models/timehseet.dart
@@ -0,0 +1,38 @@
+import 'dart:ffi';
+
+class Timesheet {
+  String? projectName;
+  ProjectDetails projectData;
+
+  Timesheet({required this.projectName, required this.projectData});
+
+  factory Timesheet.fromJson(Map<String, dynamic> json) {
+    return Timesheet(
+      projectName: json['projectName'] ?? '',
+      projectData: json['projectDetails'] != null ? ProjectDetails.fromJson(json['projectDetails']) : ProjectDetails.fromJson({}),
+    );
+  }
+}
+
+class ProjectDetails {
+  double totalBillableHours;
+  double totalNonBillableHours;
+  int leaves;
+  int extraWorkingDays;
+  double totalWorkingHours;
+
+  ProjectDetails(
+      {required this.totalBillableHours,
+      required this.extraWorkingDays,
+      required this.leaves,
+      required this.totalNonBillableHours,
+      required this.totalWorkingHours});
+  factory ProjectDetails.fromJson(Map<String, dynamic> json) {
+    return ProjectDetails(
+        totalBillableHours: json['totalBillableHours'] ?? 0.0,
+        totalNonBillableHours: json['totalNonBillableHours'] ?? 0.0,
+        leaves: json['leaves'] ?? 0,
+        extraWorkingDays: json['extraWorkingDays'] ?? 0,
+        totalWorkingHours : json['totalWorkingHours'] ?? 0.0);
+  }
+}
